25e6b5db791d469be3ffe0ffb43857f52124655a
sujatha <nalluri.sujatha@gmail.com>
2026-03-23 17:48:45 +0530

    Dummy projet

---
+++ Sample/src/test/java/demo/Program1.java
@@ -0,0 +1,14 @@
+package demo;
+
+public class Program1 {
+
+	
+		public static void main(String[] args) {
+			System.out.println("Helloooooo");
+			System.out.println("Helloooooo");
+			System.out.println("Helloooooo");
+		}
+
+	}
+
+
